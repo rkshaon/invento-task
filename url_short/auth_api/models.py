508708140cb5691c657dfc0d524902864b2656ac
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-06 00:11:42 +0600

    user - model

---
+++ url_short/auth_api/models.py
@@ -0,0 +1,9 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=255)
--- url_short/url_short/settings.py
+++ url_short/url_short/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_api',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# authentication model
+AUTH_USER_MODEL = 'auth_api.User'
