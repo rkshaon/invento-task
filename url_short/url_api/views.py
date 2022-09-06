84da74c889ce7860191c6fd7c0793fa3681c9a9b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-07 00:36:07 +0600

    short url - api - done

--- url_short/url_api/views.py
+++ url_short/url_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+import pyshorteners as sh
+
+
+@api_view(['POST'])
+def create_short_url(request):
+    if 'url' not in request.data:
+        return Response({
+            'status': False,
+            'message': 'URL required to create short URL!'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    s = sh.Shortener()
+    short_url = s.tinyurl.short(request.data['url'])
+
+    data = {
+        'short_url': short_url,
+        'url': request.data['url']
+    }
+
+    return Response({
+        'status': True,
+        'data': data,
+    })
--- url_short/url_short/all_urls.py
+++ url_short/url_short/all_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from auth_api import views as auth_views
+from url_api import views as url_views
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('login', auth_views.user_login),
     path('logout', auth_views.user_logout),
     path('profile', auth_views.user_profile),
+    path('create', url_views.create_short_url),
 ]
